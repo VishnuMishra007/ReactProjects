3a7c42b6502c8caa46b5ce04e1cce666423ca82e
vishnu mishra <vishnumishra8910@gmail.com>
2023-07-05 14:00:51 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, {useState , useEffect, useMemo, useCallback} from 'react';
 
-function App() {
+const App = () =>  {
+  const [counter,setCounter] = useState<number>(0);
+  const [name,setName] = useState<string>('');
+  useEffect(()=>{
+    setCounter(0);
+    setName('Viraaj');
+  },[]);
+
+  const inCounterHandler = () => {
+    setCounter(counter+1);
+  };
+
+  const dnCounterHandler = useCallback(() => {
+    if(counter===0)
+     return;
+    setCounter(counter-1);
+  },[counter]);
+
+
+  const dnCounterCallbackHandler = useCallback((arg1: number, arg2: number) => {
+    if(arg1===0 || arg2===0 )
+     return;
+    return arg1+arg2;
+  },[]);
+
+  const memoizedName = useMemo(()=> {
+    return name ? 'Aviraaj'+counter : '';
+  },[name,counter]);
+ 
+  //Here to return
   return (
-    <div className="App">
-     <p>Hello World</p>
+    <div className="App" style={{ padding: "10px"}}>
+     <h1>Counter Value: {counter}</h1>
+     <button id='incnt' style={{ marginRight: "10px"}} onClick={inCounterHandler}>Increase Counter</button>
+     <button id='decnt' style={{ marginRight: "10px"}} onClick={dnCounterHandler}>Decrease Counter</button>
+     <h2>{memoizedName}</h2>
+     <h2>{dnCounterCallbackHandler(2,4)}</h2>
+     <h2>{dnCounterCallbackHandler(8,4)}</h2>
+     <h2>{dnCounterCallbackHandler(7,4)}</h2>
+     <h2>{dnCounterCallbackHandler(2,4)}</h2>
     </div>
   );
 }
